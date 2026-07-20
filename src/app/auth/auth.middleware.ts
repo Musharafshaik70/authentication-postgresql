06c95c1610a5df537a67c39c59e4f2fbf576e16f
Musharaf Shaik <musharafshaik630@gmail.com>
2026-07-20 18:20:45 +0530

    define authentication middlewares

---
+++ src/app/auth/auth.middleware.ts
@@ -0,0 +1,30 @@
+import { type Request, type Response, type NextFunction, response } from "express";
+import { verifyUserToken } from "./utils/token.ts";
+
+export function authenticationMiddleware() {
+    return function (req: Request, res: Response, next: NextFunction) {
+        const header = req.headers["authorization"];
+        if (!header) return next();
+
+        if (!header.startsWith("Bearer")) return res.status(400).json({ message: "header must contain bearer" });
+
+        const token = header.split(" ")[1];
+        if (!token)
+            return res.status(400).json({ error: "authorization header must start with Bearer and followed by token" });
+
+        const user = verifyUserToken(token);
+
+        //@ts-ignore
+        req.user = user;
+
+        next();
+    };
+}
+
+export function restrictToAuthenticatedUser() {
+    return function (req: Request, res: Response, next: NextFunction) {
+        //@ts-ignore
+        if (!req.user) return res.status(401).json({ error: "Authentication Required" });
+        return next();
+    };
+}
