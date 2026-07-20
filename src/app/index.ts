df2297f8eda3da19460439d98f1b27d623b107b5
Musharaf Shaik <musharafshaik630@gmail.com>
2026-07-20 18:23:52 +0530

    Basic Auth using postgresql and drizzle completed

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,16 +1,19 @@
 import express from "express";
 import type { Express } from "express";
+import { authRouter } from "./auth/auth.routes.ts";
+import { authenticationMiddleware } from "./auth/auth.middleware.ts";
 
 function createExpressApplication(): Express {
     const app = express();
+
     app.use(express.json());
+    app.use(authenticationMiddleware());
 
-    //middlewares
-
-    //routes
     app.get("/", (req, res) => {
         return res.json({ message: "The app is working fine" });
     });
+
+    app.use("/auth", authRouter);
 
     return app;
 }
