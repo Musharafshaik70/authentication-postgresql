3f4816182ab0d2d10c6191e2c06cabb010151917
Musharaf Shaik <musharafshaik630@gmail.com>
2026-07-20 18:22:43 +0530

    add sign-in and me routes

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import type { Router } from "express";
 import AuthenticationController from "./auth.controller.ts";
+import { authenticationMiddleware, restrictToAuthenticatedUser } from "./auth.middleware.ts";
 
-export const router: Router = express.Router();
+export const authRouter: Router = express.Router();
 
 const authenticationController = new AuthenticationController();
 
-router.post("/sign-up", authenticationController.handleSignup.bind(authenticationController));
+authRouter.post("/sign-up", authenticationController.handleSignup.bind(authenticationController));
+authRouter.post("/sign-in", authenticationController.handleSignin.bind(authenticationController));
+
+authRouter.get("/me", restrictToAuthenticatedUser(), authenticationController.handleMe.bind(authenticationController));
