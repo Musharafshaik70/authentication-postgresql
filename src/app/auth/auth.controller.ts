35fd0c6691941dd9728febd9c1eb3b97ce5a5c59
Musharaf Shaik <musharafshaik630@gmail.com>
2026-07-20 18:21:26 +0530

    add handleSignIn controller

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,10 +1,72 @@
 import type { Request, Response } from "express";
-import { signupPayloadModel } from "./auth.model.ts";
+import { signinPayloadModel, signupPayloadModel } from "./auth.model.ts";
+import { db } from "../../db/index.ts";
+import { userTable } from "../../db/schema.ts";
+import { eq } from "drizzle-orm";
+import crypto from "node:crypto";
+import { createUserToken, verifyUserToken, type UserTokenPayload } from "./utils/token.ts";
 
 export default class AuthenticationController {
     public async handleSignup(req: Request, res: Response) {
         const validationResult = await signupPayloadModel.safeParseAsync(req.body);
         if (validationResult.error)
-            return res.json(400).json({ message: "body validation failed", error: validationResult.error });
+            return res.json(400).json({ message: "body validation failed", error: validationResult.error.issues });
+
+        const { firstName, lastName, email, password } = validationResult.data;
+
+        const userEmailResult = await db.select().from(userTable).where(eq(userTable.email, email));
+
+        if (userEmailResult.length > 0)
+            return res
+                .status(400)
+                .json({ error: "duplicate entry", message: `user with email ${email} already exists.` });
+
+        const salt = crypto.randomBytes(32).toString("hex");
+        const hash = crypto.createHmac("sha256", salt).update(password).digest("hex");
+
+        const [result] = await db
+            .insert(userTable)
+            .values({
+                firstName,
+                lastName,
+                email,
+                password: hash,
+                salt,
+            })
+            .returning({ id: userTable.id });
+        return res.status(201).json({ data: { id: result?.id }, message: "User has been created successfully" });
+    }
+
+    public async handleSignin(req: Request, res: Response) {
+        const validationResult = await signinPayloadModel.safeParseAsync(req.body);
+        if (validationResult.error)
+            return res.json(400).json({ message: "Failed to parse body", error: validationResult.error.issues });
+
+        const { email, password } = validationResult.data;
+
+        const [userSelect] = await db.select().from(userTable).where(eq(userTable.email, email));
+
+        if (!userSelect) return res.status(404).json({ message: `user with email ${email} does not exists` });
+
+        const salt = userSelect.salt!;
+        const hash = crypto.createHmac("sha256", salt).update(password).digest("hex");
+
+        if (userSelect.password !== hash) return res.status(400).json({ message: `email or password is incorrect` });
+
+        const token = createUserToken({ id: userSelect.id });
+        return res.status(200).json({ message: "Signin successful", data: { token } });
+    }
+
+    public async handleMe(req: Request, res: Response) {
+        // @ts-ignore
+        const { id } = req.user! as UserTokenPayload;
+
+        const [userResult] = await db.select().from(userTable).where(eq(userTable.id, id));
+
+        return res.json({
+            firstName: userResult?.firstName,
+            lastName: userResult?.lastName,
+            email: userResult?.email,
+        });
     }
 }
